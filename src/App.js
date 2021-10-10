1cb41a0d3a2efbdad207874e11aab003ec2a8b58
yelab07 <79489573+yelab07@users.noreply.github.com>
2021-10-10 18:58:06 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Header from "./containers/Header";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+} from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import ProductDetails from "./containers/ProductDetails";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Header />
+				<Switch>
+					<Route
+						path="/"
+						exact
+						component={
+							ProductListing
+						}
+					/>
+					<Route
+						path="/product/:productId"
+						component={
+							ProductDetails
+						}
+					/>
+					<Route>
+						404
+						Not
+						Found!
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
